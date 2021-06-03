042db24fc4cc167e66bbdec1ff59a3476ceef630
sabrinastangler51@gmail.com <sabrina.stangler@directsupply.com>
2021-06-03 14:17:04 -0500

    add player key press handlers

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,10 @@
-const p1 = 'red-paddle';
-const p2 = 'blue-paddle';
+// const e = require("express");
+
+const p1 = 'blue-paddle';
+const p2 = 'red-paddle';
 const ball = 'ball';
+
+
 
 app.onInit = function(){
     this.nodes.push({
@@ -30,7 +34,36 @@ app.onInit = function(){
         height : 10,
         color  : 'black'
     });
+
+    document.onkeydown = handleKeys;
+    
+    function p1UpHandler() {
+        app.getNode(p1).y-=10;
+    }
+
+    function p1DownHandler() {
+        app.getNode(p1).y+=10;
+    }
+
+    function p2UpHandler() {
+        app.getNode(p2).y-=30;
+    }
+
+    function p2DownHandler() {
+        app.getNode(p2).y+=30;
+    }
+
+    function handleKeys(ev) {
+        const callback = {
+            "KeyW"       : p1UpHandler,
+            "KeyS"       : p1DownHandler,
+            "ArrowUp"    : p2UpHandler,
+            "ArrowDown"  : p2DownHandler,
+        }[ev.code];
+        callback?.();
+    }
 };
+
 
 app.onUpdate = function(time){
     this.getNode(ball).x++;
